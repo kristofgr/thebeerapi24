768bc7a15f7f5fab92e73d47a4f91052176de452
Kristof Grenson <kristof@nizer.be>
2024-03-19 13:46:48 +0100

    use color.name instead of color.color

--- database/factories/ColorFactory.php
+++ database/factories/ColorFactory.php
@@ -17,7 +17,7 @@ class ColorFactory extends Factory
     public function definition(): array
     {
         return [
-            'color' => fake()->colorName(),
+            'name' => fake()->colorName(),
             'published' => 1
         ];
     }
--- database/migrations/2024_03_14_104337_create_colors_table.php
+++ database/migrations/2024_03_14_104337_create_colors_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
-            $table->string('color');
+            $table->string('name');
             $table->boolean('published')->default(1);
             $table->timestamps();
         });
